b384a11d65df468f6d378b36a9848bee43db9b5f
liaojinxing <jinxingliao@gmail.com>
2014-03-25 18:07:46 +0800

    Initial Commit

---
+++ Example/Example/Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by liaojinxing on 14-3-25.
+//  Copyright (c) 2014年 douban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
