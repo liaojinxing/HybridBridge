f9d0aa203ead26af2724e665f6bf5ec8f9a812ea
liaojinxing <jinxingliao@gmail.com>
2014-03-28 15:48:47 +0800

    update resource

--- Example/Example/web/js/myScript.js
+++ Example/Example/web/js/myScript.js
@@ -2,26 +2,26 @@
 var movie;
 
 function renderTemplate () {
-    renderMovieBriefSection();
-    renderMovieDetailSection();
+  renderMovieBriefSection();
+  renderMovieDetailSection();
 }
 
 function renderMovieBriefSection () {
-    var tplMoiveHeader = $("#Tpl-movie-header").text(),
-        html = $(TemplateEngine(tplMoiveHeader, movie)),
-        movieBriefSection = $("#movie-brief-section");
-    movieBriefSection.append(html);
+  var tplMoiveHeader = $("#Tpl-movie-header").text(),
+  html = $(TemplateEngine(tplMoiveHeader, movie)),
+  movieBriefSection = $("#movie-brief-section");
+  movieBriefSection.append(html);
 }
 
 function renderMovieDetailSection () {
-    var tplMoiveDetail = $("#Tpl-movie-detail-section").text(),
-        html = $(TemplateEngine(tplMoiveDetail, movie)),
-        detailSection = $("#movie-detail-section");
-    detailSection.append(html);
+  var tplMoiveDetail = $("#Tpl-movie-detail-section").text(),
+  html = $(TemplateEngine(tplMoiveDetail, movie)),
+  detailSection = $("#movie-detail-section");
+  detailSection.append(html);
 }
 
 function wish () {
-  bridge.sendMessage('wish', '5317291', hasWished);
+  bridge.sendMessageAndCallback('wish', '5317291', hasWished);
 }
 
 function hasWished(){
@@ -31,31 +31,32 @@ function hasWished(){
 }
 
 function collect () {
-        location.href = "doubanmovie://action/collect/5317291";
+  location.href = "doubanmovie://action/collect/5317291";
 }
 
 function purchase () {
-        location.href = "doubanmovie://action/purchase/5317291";
+  location.href = "doubanmovie://action/purchase/5317291";
 }
 
 var TemplateEngine = function(html, data) {
-    var re = /{{([\s\S]+?(?=}}))?}}/g,
-        reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
-        code = 'with(obj||{}){var r=[];\n',
-        cursor = 0,
-        match;
-
-    var add = function(line, js) {
-        js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
-            (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
-        return add;
-    }
-    while (match = re.exec(html)) {
-        add(html.slice(cursor, match.index))(match[1], true);
-        cursor = match.index + match[0].length;
-    }
-    add(html.substr(cursor, html.length - cursor));
-    code += 'return r.join("");}';
-    return new Function('obj', code.replace(/[\r\t\n]/g, '')).call(null, data);
-};
+  var re = /{{([\s\S]+?(?=}}))?}}/g,
+  reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
+  code = 'with(obj||{}){var r=[];\n',
+  cursor = 0,
+  match;
+  
+  var add = function(line, js) {
+    js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+    (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+                                                    return add;
+                                                    }
+                                                    while (match = re.exec(html)) {
+                                                    add(html.slice(cursor, match.index))(match[1], true);
+                                                    cursor = match.index + match[0].length;
+                                                    }
+                                                    add(html.substr(cursor, html.length - cursor));
+                                                    code += 'return r.join("");}';
+                                                    return new Function('obj', code.replace(/[\r\t\n]/g, '')).call(null, data);
+                                                    };
+                                                    
 
